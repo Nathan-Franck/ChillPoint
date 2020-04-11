f57d03a69820c9dc2a6ff065da643f192f6218be
TacticalDan <gorksorf@gmail.com>
2020-04-11 02:37:25 -0600

    ✨ Implementing Lato font

--- source/Chillpoint.Entry.ts
+++ source/Chillpoint.Entry.ts
@@ -1,15 +1,26 @@
-import { HtmlBuilder } from './Util.HtmlBuilder';
+import { HtmlBuilder, Style } from './Util.HtmlBuilder';
 
 export namespace ChillpointEntry {
+
+    export const basicDiv: Style = {
+        display: "grid",
+        justifyItems: "center",
+        alignItems: "center",
+        textAlign: "center",
+        fontFamily: "lato",
+    };
+
     export function initializeClient() {
         console.log("hey! we're in!");
         HtmlBuilder.createChild(document.body, {
             type: "div",
             style: {
+                ...basicDiv,
+
                 backgroundColor: "green",
                 width: "50",
                 height: "50",
-                borderRadius: "5px",    
+                borderRadius: "5px",
             },
             attributes: {
                 innerHTML: "Chill Point",
