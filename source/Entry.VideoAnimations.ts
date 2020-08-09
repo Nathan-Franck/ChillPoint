0cda0edb76bc0c6afc24e260d97c9f326c385b77
TacticalDan <gorksorf@gmail.com>
2020-08-09 14:43:17 -0600

    🧹 Cleaning, compressing the armor-fire code

--- source/Entry.VideoAnimations.ts
+++ source/Entry.VideoAnimations.ts
@@ -42,54 +42,6 @@ export namespace VideoAnimations {
             premultipliedAlpha: false,
         })!;
 
-        const globals = {
-            vertices: { type: "element" },
-            texture_coords: { type: "attribute", unit: "vec2" },
-            camera_size: { type: "uniform", unit: "vec2", count: 1 },
-            texture: { type: "uniform", unit: "sampler2D", count: 1 },
-            scroll: { type: "uniform", unit: "float", count: 1 },
-            uv: { type: "varying", unit: "vec2" },
-        } as const;
-        const bind_2 = {
-            texture: await ShaderBuilder.load_texture(gl, "./images/swordly_fire.png"),
-            ripple: await ShaderBuilder.load_texture(gl, "./images/ripple.png"),
-        };
-        const material_3 = ShaderBuilder.generate_material(gl, {
-            globals,
-            vert_source: `void main(void) {
-                gl_Position = vec4((texture_coords * 2.0 - 1.0) * vec2(1.0, -1.0), 0.0, 1.0); 
-                uv = texture_coords;
-            }`,
-            frag_source: `
-            void main(void) {
-                lowp vec4 tex_color = texture2D(texture, uv);
-                gl_FragColor = tex_color;
-            }`,
-        });
-        const material_2 = ShaderBuilder.generate_material(gl, {
-            globals: {
-                ...globals,
-                ripple: { type: "uniform", unit: "sampler2D", count: 1 },
-            },
-            vert_source: `void main(void) {
-                gl_Position = vec4((texture_coords * 2.0 - 1.0) * vec2(1.0, -1.0), 0.0, 1.0); 
-                uv = texture_coords;
-            }`,
-            frag_source: `
-            void main(void) {
-                lowp vec2 ripple_uv = uv + vec2(0.0, texture2D(ripple, uv * vec2(16.0, 16.0) - vec2(scroll, 0.0)).r * 0.01);
-                lowp vec4 tex_color = texture2D(texture, ripple_uv);
-                gl_FragColor = vec4(tex_color.rgb, 1);
-            }`,
-        });
-
-        const thinger_3 = {
-            texture: await ShaderBuilder.load_texture(gl, "./images/swordly_beam.png"),
-            pattern: await ShaderBuilder.load_texture(gl, "./images/fire pattern 3.png"),
-        }
-        const thinger_4 = {
-            texture: await ShaderBuilder.load_texture(gl, "./images/swordly_armor.png"),
-        }
         const constant_binds = {
             vertices: ShaderBuilder.create_element_buffer(gl, Uint16Array.from([0, 1, 2, 2, 1, 3])),
             texture_coords: ShaderBuilder.create_buffer(gl, Float32Array.from([
@@ -102,18 +54,37 @@ export namespace VideoAnimations {
             texture: await ShaderBuilder.load_texture(gl, "./images/elemental ball 2.png"),
             camera_size: [canvas.width, canvas.height],
         } as const;
+        const flame_binds = {
+            texture: await ShaderBuilder.load_texture(gl, "./images/swordly_fire.png"),
+            ripple: await ShaderBuilder.load_texture(gl, "./images/ripple.png"),
+        };
+        const beam_binds = {
+            texture: await ShaderBuilder.load_texture(gl, "./images/swordly_beam.png"),
+            pattern: await ShaderBuilder.load_texture(gl, "./images/fire pattern 3.png"),
+        } as const;
+        const armor_binds = {
+            texture: await ShaderBuilder.load_texture(gl, "./images/swordly_armor.png"),
+        } as const;
+
+        const vert_source = `void main(void) {
+            gl_Position = vec4((texture_coords * 2.0 - 1.0) * vec2(1.0, -1.0), 0.0, 1.0); 
+            uv = texture_coords;
+        }`;
+        const constant_globals = {
+            vertices: { type: "element" },
+            texture_coords: { type: "attribute", unit: "vec2" },
+            camera_size: { type: "uniform", unit: "vec2", count: 1 },
+            texture: { type: "uniform", unit: "sampler2D", count: 1 },
+            scroll: { type: "uniform", unit: "float", count: 1 },
+            uv: { type: "varying", unit: "vec2" },
+        } as const;
         const material = ShaderBuilder.generate_material(gl, {
             globals: {
-                ...globals,
+                ...constant_globals,
                 pattern: { type: "uniform", unit: "sampler2D", count: 1 },
             },
-            vert_source: `void main(void) {
-                gl_Position = vec4((texture_coords * 2.0 - 1.0) * vec2(1.0, -1.0), 0.0, 1.0); 
-                uv = texture_coords;
-            }`,
-            frag_source: `
-
-            lowp float hue_to_uv() {
+            vert_source,
+            frag_source: `lowp float hue_to_uv() {
                 lowp vec3 color = texture2D(texture, uv).rgb;
                 lowp float min = min(min(color.r, color.g), color.b);
                 lowp float max = max(max(color.r, color.g), color.b);
@@ -141,55 +112,76 @@ export namespace VideoAnimations {
                 gl_FragColor = vec4(pattern_color.rgb / alpha, alpha);
             }`,
         });
+        const armor_material = ShaderBuilder.generate_material(gl, {
+            globals: constant_globals,
+            vert_source,
+            frag_source: `
+            void main(void) {
+                lowp vec4 tex_color = texture2D(texture, uv);
+                gl_FragColor = tex_color;
+            }`,
+        });
+        const flame_effect_material = ShaderBuilder.generate_material(gl, {
+            globals: {
+                ...constant_globals,
+                ripple: { type: "uniform", unit: "sampler2D", count: 1 },
+            },
+            vert_source,
+            frag_source: `void main(void) {
+                lowp vec2 ripple_uv = uv + vec2(0.0, texture2D(ripple, uv * vec2(16.0, 16.0) - vec2(scroll, 0.0)).r * 0.01);
+                lowp vec4 tex_color = texture2D(texture, ripple_uv);
+                gl_FragColor = vec4(tex_color.rgb, 1);
+            }`,
+        });
 
         const start_time = Date.now() / 1000;
         const animation = () => {
-            const current_time = Date.now() / 1000 - start_time;
+            const current_time = (Date.now() / 1000 - start_time);
             gl.clearColor(0, 0, 0, 1);
             gl.disable(gl.DEPTH_TEST);
             gl.clear(gl.COLOR_BUFFER_BIT);
             gl.enable(gl.BLEND)
             gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
             gl.viewport(
-                (320 - bind_2.texture.width) * .5,
-                (224 - bind_2.texture.height) * .5,
-                bind_2.texture.width,
-                bind_2.texture.height);
-            ShaderBuilder.render_material(gl, material_2, {
+                (320 - flame_binds.texture.width) * .5,
+                (224 - flame_binds.texture.height) * .5,
+                flame_binds.texture.width,
+                flame_binds.texture.height);
+            ShaderBuilder.render_material(gl, flame_effect_material, {
                 ...constant_binds,
-                ...bind_2,
+                ...flame_binds,
                 scroll: -current_time,
             });
-            [0, 1, 2, 3].map(index => {
+            gl.viewport(
+                (320 - beam_binds.texture.width) * .5,
+                (224 - beam_binds.texture.height) * .5,
+                beam_binds.texture.width,
+                beam_binds.texture.height);
+            ShaderBuilder.render_material(gl, material, {
+                ...constant_binds,
+                ...beam_binds,
+                scroll: -current_time * 0.2,
+            });
+            gl.viewport(
+                (320 - armor_binds.texture.width) * .5,
+                (224 - armor_binds.texture.height) * .5,
+                armor_binds.texture.width,
+                armor_binds.texture.height);
+            ShaderBuilder.render_material(gl, armor_material, {
+                ...constant_binds,
+                ...armor_binds,
+                scroll: -current_time,
+            });
+            [0, 1, 2].map(index => {
                 gl.viewport(
-                    141 + Math.cos(current_time * 6 + index * 3.14159 * .5 + 3.14159 / 4.0) * 80,
-                    110 + Math.sin(current_time * 8 + index  * 3.14159 * .5) * 40,
+                    141 + Math.cos(current_time * 6 + index * 3.14159 * .6666) * 60,
+                    110 + Math.sin(current_time * 6 + index  * 3.14159 * .6666) * 60,
                     32,
                     32);
                 ShaderBuilder.render_material(gl, material, {
                     ...constant_binds,
                     scroll: -current_time,
                 });
-            });
-            gl.viewport(
-                (320 - thinger_3.texture.width) * .5,
-                (224 - thinger_3.texture.height) * .5,
-                thinger_3.texture.width,
-                thinger_3.texture.height);
-            ShaderBuilder.render_material(gl, material, {
-                ...constant_binds,
-                ...thinger_3,
-                scroll: -current_time,
-            });
-            gl.viewport(
-                (320 - thinger_4.texture.width) * .5,
-                (224 - thinger_4.texture.height) * .5,
-                thinger_4.texture.width,
-                thinger_4.texture.height);
-            ShaderBuilder.render_material(gl, material_3, {
-                ...constant_binds,
-                ...thinger_4,
-                scroll: -current_time,
             });
             requestAnimationFrame(animation);
         }
