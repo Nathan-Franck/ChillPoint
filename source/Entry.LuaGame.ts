424ddf97ca83ba245cf452aa1dc2baef74d7220a
TacticalDan <gorksorf@gmail.com>
2020-11-22 23:43:48 -0700

    🔧 Importing lua std library, watching source files is fast! 🏁

--- source/Entry.LuaGame.ts
+++ source/Entry.LuaGame.ts
@@ -4,9 +4,9 @@ import { Vec2 } from "./Util.VecMath";
 const curve: SmoothCurve = { x_range: [0.0, 1.0], y_values: [0.0, 1.0, 0.0] };
 
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(val => {
-    console.log(val / 10, SmoothCurve.sample(curve, val / 10));
+    print(val / 10, SmoothCurve.sample(curve, val / 10));
 });
 
 const new_vec = Vec2.add([1, 2], [3, 4]);
 
-console.log("done!");
+print("Hey! HO!");
