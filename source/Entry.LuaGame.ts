57283e4c1f2eddd30d2d80c7c3f118c768e1350b
TacticalDan <gorksorf@gmail.com>
2020-11-22 23:43:24 -0700

    ✨ Initial Luajit tests, seems to be working good 🤷‍♂️

--- source/Entry.LuaGame.ts
+++ source/Entry.LuaGame.ts
@@ -1,5 +1,7 @@
 import { SmoothCurve } from "./Util.SmoothCurve";
 
-const curve: SmoothCurve = { x_range: [0, 1], y_values: [0, 1, 0] }
+const curve: SmoothCurve = { x_range: [0.0, 1.0], y_values: [0.0, 1.0, 0.0] };
 
-console.log(SmoothCurve.sample(curve, 0.5));
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(val => {
+    console.log(val / 10, SmoothCurve.sample(curve, val / 10));
+});
