97829b2237e858e71ce5bce230074ac91387a3ab
TacticalDan <gorksorf@gmail.com>
2021-02-03 23:22:59 -0700

    Trying out lua coroutines, seems to work fine, possibly a way to get threads started?

--- source/Lua/Game.Init.ts
+++ source/Lua/Game.Init.ts
@@ -9,5 +9,19 @@ export const window = sdl.SDL_CreateWindow(
     600,
     0, //SDL.SDL_WINDOW_FULLSCREEN, //
 )!;
+
 export const renderer = sdl.SDL_CreateRenderer(window, -1, SDL.SDL_RENDERER_ACCELERATED & SDL.SDL_RENDERER_PRESENTVSYNC )!;
 sdl.SDL_SetRenderDrawColor(renderer, 0xFF, 0, 0xFF, 0xFF);
+
+function sleep(time: number) {
+  if (time > 0) os.execute(`ping -n ${ time + 1 } localhost > NUL`)
+}
+
+const routine_example = coroutine.create((starting_thing) => {
+    print(`Hello from another thread! ${starting_thing}`);
+    const [result_from_main] = coroutine.yield("result from thread");
+    print(`Hello from another thread! ${result_from_main}`);
+});
+
+coroutine.resume(routine_example, "hello!");
+coroutine.resume(routine_example, "hey_there!");
